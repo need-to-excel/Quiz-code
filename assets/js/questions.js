50825411cb8a8079895d29a2afef47fd27c0fedf
Betelgeuse <abdumavlyanov2004@yahoo.com>
2024-01-12 19:15:45 +0000

    starter code

---
+++ assets/js/questions.js
@@ -0,0 +1,21 @@
+var questionsBank = [
+    {
+      question: "Why variables used in Javascript?",
+      answers: {
+        a: "To store information",
+        b: "To process information",
+        c: "To display information"
+      },
+      correctAnswer: 'a'
+    },
+    {
+      question: "What functions do in Javascript?",
+      answers: {
+        a: "",
+        b: '5',
+        c: '10'
+      },
+      correctAnswer: 'c'
+    }
+  ];
+  
